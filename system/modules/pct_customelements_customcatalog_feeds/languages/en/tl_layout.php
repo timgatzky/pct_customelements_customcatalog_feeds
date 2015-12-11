09256b20477d8836b2e91669058477114035b475
Tim Gatzky <info@tim-gatzky.de>
2015-12-11 10:38:20 +0100

    update

---
+++ system/modules/pct_customelements_customcatalog_feeds/languages/en/tl_layout.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_layout']['customcatalogfeeds'][0] = 'CustomCatalog-Feeds';
+$GLOBALS['TL_LANG']['tl_layout']['customcatalogfeeds'][1] = 'Choose the CustomCatalog feeds you want to add to this page layout';
---
+++ system/modules/pct_customelements_customcatalog_feeds/languages/en/tl_pct_customcatalog_feed.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['new'][0] = 'New feed';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['new'][1] = 'Creates a new feed';
+
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['configs'][0] 			= 'Configurations';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['configs'][1] 			= 'Choose the configurations for this feed';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['description'][0] 		= 'Feed description';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['description'][1] 		= 'Please enter a short description of this feed';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['jumpTo'][0] 			= 'Redirect page';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['jumpTo'][1] 			= 'Please choose the reader page to which visitors will be redirected when clicking an item.';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['titleField'][0] 		= 'Titel';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['titleField'][1] 		= 'Please choose the field/attribute that contains the title';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['publishedField'][0] 	= 'Date of publication';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['publishedField'][1] 	= 'Please choose the field/attribute that contains the date of publication';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['descriptionField'][0] = 'Description / content';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['descriptionField'][1] = 'Please choose the field/attribute that contains the main content';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['imageField'][0] 		= 'Image attachmend';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['imageField'][1] 		= 'Please choose the field/attribute that contains an image source';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['authorField'][0] 		= 'Author';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['authorField'][1] 		= 'Please choose the field/attribute that contains the author';
+
+
+/**
+ * Legend
+ */
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['configs_legend'] 	= 'Configurations';
+$GLOBALS['TL_LANG']['tl_pct_customcatalog_feed']['config_legend'] 	= 'Feed settings';
