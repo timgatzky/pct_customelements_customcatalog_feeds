0ebf0b649dfbf9ab2674503cdb67647802b2a9f8
Tim Gatzky <info@tim-gatzky.de>
2016-11-28 10:54:27 +0100

    added missing fields

---
+++ system/modules/pct_customelements_customcatalog_feeds/dca/tl_user_group.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * 
+ * Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * @copyright	Tim Gatzky 2016
+ * @author		Tim Gatzky <info@tim-gatzky.de>
+ * @package		pct_customelements
+ * @subpackage	pct_customelements_plugin_customcatalog
+ * @subpackage	pct_customelements_customcatalog_feeds
+ * @link		http://contao.org
+ */
+
+
+/**
+ * Extend default palette
+ */
+$GLOBALS['TL_DCA']['tl_user_group']['palettes']['default'] = str_replace('fop;', 'fop;{customcatalogfeeds_legend},customcatalogfeeds,customcatalogfeedp;', $GLOBALS['TL_DCA']['tl_user_group']['palettes']['default']);
+
+
+/**
+ * Add fields to tl_user_group
+ */
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['customcatalogfeeds'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_user']['customcatalogfeeds'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'foreignKey'              => 'tl_pct_customcatalog_feed.title',
+	'eval'                    => array('multiple'=>true),
+	'sql'                     => "blob NULL"
+);
+
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['customcatalogfeedp'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_user']['customcatalogfeedp'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'options'                 => array('create', 'delete'),
+	'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+	'eval'                    => array('multiple'=>true),
+	'sql'                     => "blob NULL"
+);
